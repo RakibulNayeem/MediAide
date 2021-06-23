d4b08900af0492c81e4ce078b50efec0009410dd
Abduz Zami <69592754+AbduzZami@users.noreply.github.com>
2021-06-23 21:26:43 +0600

    commit-az

---
+++ app/src/main/java/com/rakibulnayeem/mediaide/fcmnoti/FcmNotificationsSender.java
@@ -0,0 +1,96 @@
+package com.rakibulnayeem.mediaide.fcmnoti;
+
+import android.app.Activity;
+import android.content.Context;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+import com.rakibulnayeem.mediaide.R;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class FcmNotificationsSender  {
+
+    String userFcmToken;
+    String title;
+    String body;
+    Context mContext;
+    Activity mActivity;
+
+
+    private RequestQueue requestQueue;
+    private final String postUrl = "https://fcm.googleapis.com/fcm/send";
+    private final String fcmServerKey ="AAAA9epvgoY:APA91bGmb09tA9uKi7xh8Ot_qPbSF8m7oT4ckMHElbyfgovG7voW67m5mcO1nedLPA1clqQ1j5uFGXrq-b63A-HjXAnBHMPTNB6g_IWZn2VWyE6EhWCgljB95LwJe5gXqtR-jshvhkbQ";
+
+    public FcmNotificationsSender(String userFcmToken, String title, String body, Context mContext, Activity mActivity) {
+        this.userFcmToken = userFcmToken;
+        this.title = title;
+        this.body = body;
+        this.mContext = mContext;
+        this.mActivity = mActivity;
+
+
+    }
+
+    public void SendNotifications() {
+
+        requestQueue = Volley.newRequestQueue(mActivity);
+        JSONObject mainObj = new JSONObject();
+        try {
+            mainObj.put("to", userFcmToken);
+            JSONObject notiObject = new JSONObject();
+            notiObject.put("title", title);
+            notiObject.put("body", body);
+            notiObject.put("icon", R.drawable.ic_launcher_foreground);
+
+
+
+            mainObj.put("notification", notiObject);
+
+
+            JsonObjectRequest request = new JsonObjectRequest(Request.Method.POST, postUrl, mainObj, new Response.Listener<JSONObject>() {
+                @Override
+                public void onResponse(JSONObject response) {
+
+
+                }
+            }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+
+
+                }
+            }) {
+                @Override
+                public Map<String, String> getHeaders() throws AuthFailureError {
+
+
+                    Map<String, String> header = new HashMap<>();
+                    header.put("content-type", "application/json");
+                    header.put("authorization", "key=" + fcmServerKey);
+                    return header;
+
+
+                }
+            };
+            requestQueue.add(request);
+
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+
+
+
+    }
+}
---
+++ app/src/main/java/com/rakibulnayeem/mediaide/fcmnoti/NotifyMembers.java
@@ -0,0 +1,117 @@
+package com.rakibulnayeem.mediaide.fcmnoti;
+
+import android.app.Activity;
+import android.content.Context;
+
+import androidx.annotation.NonNull;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.messaging.FirebaseMessaging;
+import com.niharon.classmanager.model.Classs;
+import com.niharon.classmanager.model.Member;
+
+import java.util.ArrayList;
+
+public class NotifyMembers {
+    public static void sendNotification(String title, String message, String roomid, String classid, String day, Context context, Activity activity)
+    {
+        try {
+            DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference("Rooms").child(roomid).child("Schedule").child(day).child(classid);
+            databaseReference.addListenerForSingleValueEvent(new ValueEventListener() {
+                @Override
+                public void onDataChange(@NonNull DataSnapshot snapshot) {
+                    Classs classs = snapshot.getValue(Classs.class);
+                    ArrayList<String> liste = new ArrayList<>();
+                    DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference("Rooms").child(roomid).child("Members");
+                    databaseReference.addListenerForSingleValueEvent(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(@NonNull DataSnapshot snapshot) {
+                            for (DataSnapshot dataSnapshot:snapshot.getChildren()
+                            ) {
+                                Member member = dataSnapshot.getValue(Member.class);
+                                assert member != null;
+                                if (!member.getId().equals(FirebaseAuth.getInstance().getCurrentUser().getUid()))
+                                {
+                                    if (!member.getRole().equals("teacher"))
+                                    {
+                                        liste.add(member.getId());
+                                    }
+                                    else {
+                                        assert classs != null;
+                                        if (member.getEmail().equals(classs.getClassteacheremail())) {
+                                            liste.add(member.getId());
+                                        }
+                                    }
+                                }
+                            }
+                            String[] lists = new String[liste.size()];
+                            for (int i = 0; i < liste.size(); i++) {
+                                lists[i] = liste.get(i);
+                            }
+
+                            for (String userid:lists
+                            ) {
+                                try{
+                                    FirebaseDatabase.getInstance().getReference().child("Tokens").child(userid).child("token").addListenerForSingleValueEvent(new ValueEventListener() {
+                                        @Override
+                                        public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+                                            try
+                                            {
+                                                String usertoken=dataSnapshot.getValue(String.class);
+                                                FcmNotificationsSender notificationsSender = new FcmNotificationsSender(usertoken,title,message,context,activity);
+                                                notificationsSender.SendNotifications();
+                                            }
+                                            catch (Exception ignored){}
+                                        }
+
+                                        @Override
+                                        public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                                        }
+                                    });
+                                }catch (Exception ignored){}
+                            }
+                        }
+
+                        @Override
+                        public void onCancelled(@NonNull DatabaseError error) {
+
+                        }
+                    });
+                }
+
+                @Override
+                public void onCancelled(@NonNull DatabaseError error) {
+
+                }
+            });
+        }catch (Exception ignored){}
+    }
+
+    static public void UpdateToken(){
+        FirebaseUser firebaseUser= FirebaseAuth.getInstance().getCurrentUser();
+        FirebaseMessaging.getInstance().getToken()
+                                .addOnCompleteListener(new OnCompleteListener<String>() {
+                                    @Override
+                                    public void onComplete(@NonNull Task<String> task) {
+                                        if (!task.isSuccessful()) {
+                                            return;
+                                        }
+
+                                        // Get new FCM registration token
+                                        String refreshtoken = task.getResult();
+                                        Token token = new Token(refreshtoken);
+                                        FirebaseDatabase.getInstance().getReference("Tokens").child(FirebaseAuth.getInstance().getCurrentUser().getUid()).setValue(token);
+                                    }
+                                });
+    }
+}
